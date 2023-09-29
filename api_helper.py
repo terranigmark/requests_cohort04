09a02fe5df3d3fd0faa8aefc5dd8952673fd45be
terranigmark <linnk99@gmail.com>
2023-09-28 19:29:23 -0600

    Funcion para generar posts creada

--- api_helper.py
+++ api_helper.py
@@ -5,3 +5,11 @@ import requests
 def get_posts():
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
     return response
+
+def create_post(title, body, userId):
+    payload = {
+        'title': title,
+        'body': body,
+        'userId': userId
+    }
+    response = requests.post('https://jsonplaceholder.typicode.com/posts', json=payload)
