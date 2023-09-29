ed95fe620b91c15e8ad6937b480e632e147bb6bc
terranigmark <linnk99@gmail.com>
2023-09-28 19:24:10 -0600

    Funcion para traer posts creada

--- api_helper.py
+++ api_helper.py
@@ -1,3 +1,7 @@
 # Este archivo contiene los llamados a la API JSON Placeholder
+import requests
 
 
+def get_posts():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    return response
